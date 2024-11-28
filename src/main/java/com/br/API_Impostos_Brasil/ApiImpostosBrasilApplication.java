16d1ebf5bfcf84c778dd27165fb83c44397ad1e3
Duda <maria.oliveira@zup.com.br>
2024-11-28 15:32:16 -0300

    first commit

---
+++ src/main/java/com/br/API_Impostos_Brasil/ApiImpostosBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.br.API_Impostos_Brasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiImpostosBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiImpostosBrasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/API_Impostos_Brasil/ApiImpostosBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.API_Impostos_Brasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiImpostosBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
